dc37154547cb66bed126cc5e81519c7c7021a100
telegrambot81 <telegrambot81@gmail.com>
2016-04-03 15:25:38 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {120629055,106789762},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
@@ -235,11 +235,11 @@ An advance Administration bot based on yagop/telegram-bot
 https://github.com/SEEDTEAM/TeleSeed
 
 Our team!
-Alphonse (@Iwals)
-I M /-\ N (@Imandaneshi)
-Siyanew (@Siyanew)
-Rondoozle (@Potus)
-Seyedan (@Seyedan25)
+Alphonse (...)
+I M /-\ N (...)
+Siyanew (....)
+Rondoozle (...)
+Seyedan (....)
 
 Special thanks to:
 Juan Potato
@@ -248,8 +248,8 @@ Topkecleon
 Vamptacus
 
 Our channels:
-English: @TeleSeedCH
-Persian: @IranSeed
+English: ....
+Persian: ....
 ]],
     help_text_realm = [[
 Realm Commands:
